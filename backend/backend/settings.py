ff96fcc69b45436ccc89975540772ef70bf58b8e
Михаил Хохлов <michelin_90@mail.ru>
2023-07-07 14:56:14 +0500

    add deploy to wf

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -4,9 +4,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'backend',
+    'db',
+    'testserver',
+    '158.160.68.141'
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
